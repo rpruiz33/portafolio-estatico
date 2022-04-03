a51f2e5a6f6cbfa1a1fd133005824ca56b87cd19
rpruiz33 <rpruiz33@gmail.com>
2022-04-03 13:18:29 -0300

    first commit

---
+++ probando.write.js
@@ -0,0 +1,6 @@
+var miArray = new Array();
+miArray[0] = 155;
+miArray[1] = 499;
+miArray[2] = 65;
+document.write("Longitud del array: " + miArray.length);
+console.log("Longitud del array: " + miArray.length);
